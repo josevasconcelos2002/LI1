0ffff791d9e84a817ac4d665c4c859d6baa5e93b
José Pedro Torres Vasconcelos <a100763@alunos.uminho.pt>
2022-10-09 19:42:40 +0100

    Alterações

--- aulas/aula1/HelloWorld.hs
+++ aulas/aula1/HelloWorld.hs
@@ -1,2 +1,9 @@
+{-
+Module : Main
+Descpription : Módulo Haskell Hello World
+Copyright : José Vasconcelos <a100763@uminho.pt>;
+-}
 module Main where
+{- |A função 'main' imprime o texto @"Hello World!"@
+-}
 main = do putStrLn "Hello World!"
